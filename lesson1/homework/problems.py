f9767ec868996f85e074476b95d89f1fdcb03d5c
JacobHaile345 <hgwubneh23@gmail.com>
2026-02-19 20:52:25 -0800

    Update problems.py

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,31 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+y = 67 #: whole numbers without a decimal
+print(y)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+s = 7 #: whole numbers without a decimal
+print(s)
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
+favorite_fruit ="strawberry" # string: text enclosed in quotes 
+print (favorite_fruit)
 
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
+city ="seattle"city ="seattle" # string: text enclosed in quotes
+print (city)
+ country ="united states ofamerica"country ="united states ofamerica" # string: text enclosed in quotes
+print (country)
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+name = "Bob" # string: text enclosed in quotes 
+age = 2 #: whole numbers without a decimal
+type = "Golden Retriever" # string: text enclosed in quotes
+print(name) print(age) print(type)
