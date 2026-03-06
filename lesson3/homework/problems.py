9f47ba88fdb1b95c1682fc27aaa91e62fafc1858
JacobHaile345 <hgwubneh23@gmail.com>
2026-03-05 20:13:51 -0800

    Update problems.py

--- lesson3/homework/problems.py
+++ lesson3/homework/problems.py
@@ -1,31 +1,34 @@
 # Problem 1
 # Ask the user to enter a number.
 # Print "Even" if the number is divisible by 2, otherwise print "Odd".
-
-
+number = int(input("Please enter an number: ")
+print("Even")
 
 # Problem 2
 # Ask the user for the day of the week (all lowercase).
 # Print "Weekend" if the day is "saturday" or "sunday",
 # else print "Weekday".
-
-
+day = input("Enter the day of the week (all lowercase): ")
+print("Weekday")
 
 # Problem 3
 # Generate a random number between 1 and 10 (inclusive).
 # Ask the user to guess the number.
 # Print "Correct!" if the guess matches the random number, else print "Try again!".
-
-
-
+random_number = random.randint(1, 10)
+user_guess = int(input("Guess a number between 1 and 10: "))
+print("Correct!")
 # Problem 4
 # Ask the user for a positive integer.
 # If the number is divisible by 2 and greater than 10, print "Big even number".
 # Otherwise print "Number does not meet criteria".
-
+  number = int(input("Enter a positive integer: "))
+print("Big even number")
 
 
 # Problem 5
 # Ask the user for two numbers.
 # Print which number is larger.
 # If the numbers are equal, print "Numbers are equal".
+number = input("Enter the first number: ")
+number_2 = input("Enter the second number: ")
