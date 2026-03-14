024b84136de74a7585283f706e15f313e8c73f60
JacobHaile345 <hgwubneh23@gmail.com>
2026-03-13 20:46:34 -0700

    Update problems.py

--- lesson4/homework/problems.py
+++ lesson4/homework/problems.py
@@ -2,23 +2,24 @@
 # Ask user for two test scores.
 # If BOTH scores are at least 50, print "You passed both!"
 # Otherwise, print "You failed at least one."
-
-
+score1 = float(input("Enter test score 1: "))
+score2 = float(input("Enter test score 2: "))
+    print("You passed both!")
 
 # Problem 2
 # Ask user if they brought lunch and water (yes/no).
 # If they brought lunch OR water, print "You're somewhat ready."
 # If they brought both, print "You're fully ready!"
 # If they brought neither, print "You're not ready."
-
-
+brought_items = input("Did you bring lunch and water? (yes/no): ")
+ print("You're somewhat ready.")
 
 # Problem 3
 # Ask user to enter a number.
 # If the number is NOT between 1 and 10 (inclusive), print "Out of range."
 # Otherwise, print "In range."
-
-
+num = int(input("Please enter a number: "))
+print("Out of Range")
 
 # Problem 4
 # Generate a random number between 1 and 10.
@@ -26,10 +27,14 @@
 # If the guess is right AND the number is even, print "Even match!"
 # Else if guess is right OR number is 5, print "Nice try!"
 # Otherwise, print "Nope."
-
-
+random_number = random.randint(1, 10)
+user_guess = int(input("Guess a number between 1 and 10: "))
+print(even match)
 
 # Problem 5
 # Ask the user for two numbers.
 # If one is divisible by 5 AND the other is NOT divisible by 2, print "Interesting pair!"
 # Otherwise, print "Plain pair."
+first_number_str = input("Enter the first number: ")
+second_number_str = input("Enter the second number: ")
+print("Intresting pair")
